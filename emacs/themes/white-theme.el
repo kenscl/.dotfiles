1268c230d37ff70dbb20712bbe8cc8c11101c5de
ken <kenschlesiger@gmail.com>
2025-09-09 14:18:16 +0200

    stuff

--- emacs/themes/white-theme.el
+++ emacs/themes/white-theme.el
@@ -11,32 +11,54 @@
   (w-l-gray     "#666666")
   (w-blue       "#276FBF")
   (w-purple     "#A63446")
+  (w-ll-gray    "#C2C2C2")
+  (w-viridian   "#2A7F62")
   )
 
- ;; Customize faces
- ((default                   (:foreground w-black :background w-white))
-  (cursor                    (:background w-gray))
-  (region                    (:background w-gray))
-  (fringe                    (:background w-white))
-  (mode-line                 (:background w-gray :foreground w-black))
-  (mode-line-inactive        (:background w-l-gray :foreground w-gray))
-  (minibuffer-prompt         (:foreground w-blue))
+ ;; Faces
+ ((default                        (:foreground w-black :background w-white))
+  (cursor                         (:background w-gray))
+  (region                         (:background w-ll-gray))
+  (fringe                         (:background w-white))
+  
+  ;; --- Mode line / minibuffer ---
+  (mode-line                      (:background w-black :foreground w-white))
+  (mode-line-inactive             (:background w-ll-gray :foreground w-gray))
+  (mode-line-highlight            (:foreground w-blue :weight 'bold))
+  (mode-line-buffer-id            (:foreground w-purple :weight 'bold))
+  (mode-line-emphasis             (:foreground w-blue))
+  (minibuffer-prompt              (:foreground w-blue :weight 'bold))
 
-  ;; Syntax
-  (font-lock-keyword-face    (:foreground w-blue))
-  (font-lock-constant-face   (:foreground w-blue))
-  (font-lock-string-face     (:foreground w-gray))
-  (font-lock-builtin-face    (:foreground w-l-gray))
-  (font-lock-comment-face    (:foreground w-l-gray))
+  (mode-line-modes            (:foreground w-black :background w-black))
+  (evil-normal-state-tag      (:foreground w-white :background w-blue :weight 'bold))
+  (evil-insert-state-tag      (:foreground w-white :background w-purple :weight 'bold))
+  (evil-visual-state-tag      (:foreground w-white :background w-l-gray :weight 'bold))
+  (evil-replace-state-tag     (:foreground w-white :background w-purple :weight 'bold))
+  (evil-motion-state-tag      (:foreground w-white :background w-blue :weight 'bold))
+  (evil-operator-state-tag    (:foreground w-white :background w-l-gray :weight 'bold))
+
+  ;; Magit minor modes
+  (magit-section-heading      (:foreground w-blue :weight 'bold))
+  (magit-branch-local         (:foreground w-purple :weight 'bold))
+  (magit-branch-remote        (:foreground w-blue :weight 'bold))
+  (magit-diff-added           (:foreground w-viridian))
+  (magit-diff-removed         (:foreground w-purple))
+
+  ;; --- Syntax highlighting ---
+  (font-lock-keyword-face         (:foreground w-blue))
+  (font-lock-constant-face        (:foreground w-blue))
+  (font-lock-string-face          (:foreground w-gray))
+  (font-lock-builtin-face         (:foreground w-l-gray))
+  (font-lock-comment-face         (:foreground w-l-gray))
   (font-lock-comment-delimiter-face (:inherit font-lock-comment-face))
-  (font-lock-type-face          (:foreground w-purple :weight 'bold))   ;; int, char, etc.
-  (font-lock-variable-name-face (:foreground w-gray))                 ;; variable names
-  (font-lock-function-name-face (:foreground w-blue))         
+  (font-lock-type-face            (:foreground w-purple :weight 'bold))
+  (font-lock-variable-name-face   (:foreground w-gray))
+  (font-lock-function-name-face   (:foreground w-blue))
 
-  ;; Org
-  (org-level-1               (:foreground w-blue :height 1.3 :weight 'bold))
-  (org-level-2               (:foreground w-purple :height 1.2 :weight 'bold))
-  (org-level-3               (:foreground w-l-gray :height 1.1 :weight 'bold))
-  (org-level-4               (:foreground w-gray :height 1.1 :weight 'bold))))
+  ;; --- Org mode ---
+  (org-level-1                    (:foreground w-blue :height 1.3 :weight 'bold))
+  (org-level-2                    (:foreground w-purple :height 1.2 :weight 'bold))
+  (org-level-3                    (:foreground w-l-gray :height 1.1 :weight 'bold))
+  (org-level-4                    (:foreground w-gray :height 1.1 :weight 'bold))))
 
 (provide-theme 'white)
